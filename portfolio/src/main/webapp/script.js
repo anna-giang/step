277cc64db92ff801ff2dc2c831e5d84413c5ba03
Anna Giang <anna.giang1999@gmail.com>
2020-12-10 23:18:28 +0000

    Fix formatting of script.js according to style guide

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,11 +17,12 @@
  * @returns none
  */
 function addRandomFact() {
-  const facts =
-    ["I have two younger brothers both about a foot taller than me.", 
-     "My parents are from Vietnam but I don't speak any Vietnamese.",
-     "I have never been to anywhere else in Australia except Melbourne, where I was born and raised."
-    ];
+  const facts = [
+    "I have two younger brothers both about a foot taller than me.", 
+    "My parents are from Vietnam but I don't speak any Vietnamese.",
+    "I have never been to anywhere else in Australia except Melbourne, \
+    where I was born and raised."
+  ];
 
   // Pick a random fact.
   const fact = facts[Math.floor(Math.random() * facts.length)];
@@ -37,17 +38,17 @@ function addRandomFact() {
  * @returns none
  */
 function toggleBlogPost(buttonId) {
-    // Remove the '-b' from the button ID to get the ID of div of blog post.
-    var blogDivId = buttonId.slice(0, buttonId.length-2);
-    var content = document.getElementById(blogDivId);
-    var button = document.getElementById(buttonId);
+  // Remove the '-b' from the button ID to get the ID of div of blog post.
+  var blogDivId = buttonId.slice(0, buttonId.length-2);
+  var content = document.getElementById(blogDivId);
+  var button = document.getElementById(buttonId);
 
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-        // Change the label in the button accordingly to what button will do
-        button.innerHTML = "Read less";
-    } else {
-        content.style.display = "none";
-        button.innerHTML = "Read more";
-    }
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+    // Change the label in the button accordingly to what button will do
+    button.innerHTML = "Read less";
+  } else {
+    content.style.display = "none";
+    button.innerHTML = "Read more";
+  }
 }
