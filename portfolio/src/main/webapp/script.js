f176a8b782dc284d080d8d32e3dfe6156775eb5e
Anna Giang <49330921+anna-giang@users.noreply.github.com>
2020-12-11 12:24:41 +1100

    Starter Project 1 - Implementation for Portfolio website front-end
    * Update the README.md file to describe the repo.
    
    * Add empty pages for portfolio sections
    
    * Personalise index.html - add images that link to individual portfolio sections on index.html and section descriptions.
    
    * Add text content and random fact generator for aboutMe page
    
    * Add content for projects.html page. Showcasing some of my side projects.
    
    * Add content for the blog.html page
    
    Initally hide blog posts on render.
    Add button to toggle show/hide of blog posts, linked to toggleBlogPost function in script.js.
    
    * Add content for gallery.html
    
    10 images arranged in a picture gallery created using CSS flexbox.
    
    * Change arrangement of page elements on index, projects and blog
    
    Use flexboxes to arrange each section of the page into rows and columns.
    
    * Arrange elements on the aboutMe.html page
    
    * Modify second blog post content in blog.html
    
    Second blog post previously contained test content.
    
    * Blur linked images on hover and open external links in new tab
    
    Images that can be clicked are "blurred" (reduce opacity) on mouse-over/hover to indicate that they can be clicked.
    Also rename "section-pic" to "section-img" for consistency.
    
    * Adjust font-family and font-size for all text
    
    * Add footer with links to GitHub, LinkedIn and Email
    
    * Add padding around buttons to create space from adjacent elements
    
    * Style buttons
    
    * Fix format of CSS file according to style guide
    
    * Fix style.css formatting according to the style guide.
    
    * Fix formatting of HTML code according to style guide
    
    * Add alternative contents for all images
    
    * Fix formatting and comments in script.js and delete unused code.
    
    * Fix formatting and fix minor content mistakes
    
    - 2-space indents instead of tabs across all files
    - Fix comments in script.js - add param type between {} in function header
    - Blank line at the end of every file
    - Fix mispelling "Hear more about *my* the..." in index.html
    - Close <div id="content"> in gallery.html
    - Modify <ul> in aboutMe.html - nest  all <span> under <li>
    
    * Fix formatting of script.js according to style guide
    
    * Replace double quotes with single quotes in script.js for string literals
    
    * Add blank line at end of gallery.html

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,42 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random fact to the page.
+ * @returns none
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomFact() {
+  const facts = [
+    'I have two younger brothers both about a foot taller than me.', 
+    'My parents are from Vietnam but I don\'t speak any Vietnamese.',
+    'I have never been to anywhere else in Australia except Melbourne, \
+    where I was born and raised.'
+  ];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random fact.
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
+}
+
+/**
+ * Show the full blog post text for blog post div with the given ID.
+ * @param {String} blogId ID of the div of the blog post to be shown.
+ * @returns none
+ */
+function toggleBlogPost(buttonId) {
+  // Remove the '-b' from the button ID to get the ID of div of blog post.
+  var blogDivId = buttonId.slice(0, buttonId.length-2);
+  var content = document.getElementById(blogDivId);
+  var button = document.getElementById(buttonId);
+
+  if (content.style.display === 'none' || content.style.display === "") {
+    content.style.display = 'block';
+    // Change the label in the button accordingly to what button will do
+    button.innerHTML = 'Read less';
+  } else {
+    content.style.display = 'none';
+    button.innerHTML = 'Read more';
+  }
 }
