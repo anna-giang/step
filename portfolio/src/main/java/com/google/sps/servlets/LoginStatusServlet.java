2899ef74ab9b67e0af7831380c8c864f430f45e0
Anna Giang <49330921+anna-giang@users.noreply.github.com>
2020-12-23 09:16:56 +1100

    Add servlet to check login status of user (#12)
    * Add a servlet that returns the login status of user
    
    Servlet will return a JSON with loggedIn=true or false, and a logoutUrl or loginUrl as appropriate.
    
    * Fix spelling mistake logoutUrl -> loginUrl
    
    * remove unnecessary blank lines

---
+++ portfolio/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
@@ -0,0 +1,54 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.HashMap;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Returns JSON containing logged in status and login/logout links as appropriate */
+@WebServlet("/login-status")
+public class LoginStatusServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+
+    UserService userService = UserServiceFactory.getUserService();
+    HashMap<String, String> userData = new HashMap<String, String>();
+
+    if (userService.isUserLoggedIn()) {
+      String logoutRedirectUrl = "/index.html";
+      String logoutUrl = userService.createLogoutURL(logoutRedirectUrl);
+      userData.put("loggedIn", "true");
+      userData.put("logoutUrl", logoutUrl);
+    } else {
+      String loginRedirectUrl = "/index.html";
+      String loginUrl = userService.createLoginURL(loginRedirectUrl);
+      userData.put("loggedIn", "false");
+      userData.put("loginUrl", loginUrl);
+    }
+    
+    // Convert to JSON and return
+    Gson gson = new Gson();
+    response.getWriter().println(gson.toJson(userData));
+  }
+}
