20361a945267b5ac08ecf0b9bedddf3ad90fc0c7
Anna Giang <anna.giang1999@gmail.com>
2020-12-23 04:39:52 +0000

    Add servlet, and form on front-end for user to set nickname
    NicknameServlet doGet will prompt the user to login (if not logged in) and will redirect the user to the createUserProfile.html to input a nickname if they do not have a nickname, before redirecting back to index.html.
    Form on createUserProfile.html allows user to submit a nickname, and is linked to the doPost in the NicknameServlet, which will store the chosen nickname under a UserInfo entity in Datastore.

---
+++ portfolio/src/main/java/com/google/sps/servlets/NicknameServlet.java
@@ -0,0 +1,95 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import java.io.IOException;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Fetch and set the user nickname */
+@WebServlet("/nickname")
+public class NicknameServlet extends HttpServlet {
+  
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    if (!userService.isUserLoggedIn()) { // If user not logged in, redirect them to login before coming back
+      String loginUrl = userService.createLoginURL("/nickname");
+      response.sendRedirect(loginUrl);
+      return;
+    }
+
+    String nickname = getUserNickname(userService.getCurrentUser().getUserId());
+    if (nickname == null) {
+      response.sendRedirect("/createUserProfile.html");
+      return;
+    } 
+
+    // User is logged in and their nickname is set, go back to home
+    response.sendRedirect("/index.html");
+  }
+
+  /** Sets a nickname for the user */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) { // If user not logged in, redirect them to the login page
+      String loginUrl = userService.createLoginURL("/nickname");
+      response.sendRedirect(loginUrl);
+      return;
+    }
+
+    String nickname = request.getParameter("nickname");
+    String id = userService.getCurrentUser().getUserId();
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity entity = new Entity("UserInfo", id);
+    entity.setProperty("id", id);
+    entity.setProperty("nickname", nickname);
+    datastore.put(entity);
+
+    response.sendRedirect("/index.html");
+  }
+
+  /** Returns the nickname of the user with id, or null if the user has not 
+   * set a nickname. 
+   * @param id user id of the user.
+   */
+  private String getUserNickname(String id) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = 
+        new Query("UserInfo")
+            .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, id));
+    PreparedQuery results = datastore.prepare(query);
+    Entity entity = results.asSingleEntity();
+    if (entity == null) {
+      return null;
+    }
+    String nickname = (String) entity.getProperty("nickname");
+    return nickname;
+  }
+}
